6fca2bf249a6d7e9dbb69f3c99024e885d44a2ea
Connor <Connorh223@gmail.com>
2021-08-16 18:57:17 -0400

    added stuff

--- django_music/settings.py
+++ django_music/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'django_music.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
